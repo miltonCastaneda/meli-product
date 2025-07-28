e10af4efa9c37372b1dd99ea28fce11727e90dfb
Milton Castañeda <milton1792@gmail.com>
2025-07-28 06:45:33 -0500

    FRONTEND - se ajustan pruebas de integracion E2E

--- frontend/tests/product-detail.spec.js
+++ frontend/tests/product-detail.spec.js
@@ -16,23 +16,6 @@ test.describe('Product Detail Page', () => {
     // Assert product price
     await expect(page.locator('div.flex.items-baseline span.text-3xl')).toHaveText('US$ 439');
 
-    // Assert product condition and sold quantity
-    // await expect(page.locator('div.text-xs.text-gray-500.mb-2.flex.items-center')).toContainText('Nuevo | 1500 vendidos');
-
-    // Assert seller name
-    // await expect(page.locator('a.text-blue-500.hover\:text-blue-700')).toHaveText('Visita la Tienda oficial de Samsung');
-
-    // // Assert description presence
-    // await expect(page.locator('h2.text-2xl.font-light.text-gray-700.mb-4')).toHaveText('Descripción');
-    // await expect(page.locator('p.text-gray-600.whitespace-pre-line.text-base')).toContainText('Capacidad y eficiencia');
-
-    // // Assert image gallery presence
-    // await expect(page.locator('div.flex-1.flex.items-center.justify-center img').first()).toBeVisible();
-
-    // // Assert key features presence
-    // await expect(page.locator('h3.font-semibold.mb-2:has-text("Lo que tienes que saber de este producto")')).toBeVisible();
-    // await expect(page.locator('ul.list-disc.list-inside.text-sm.space-y-2.text-gray-600 li').first()).toHaveText('Memoria RAM: 8 GB');
-
     // Assert characteristics presence
     await expect(page.locator('h2.text-2xl.font-light.text-gray-700.mb-6')).toHaveText('Características del producto');
     await expect(page.locator('div.grid.grid-cols-1.md\\:grid-cols-2 p.font-semibold').first()).toContainText('Tamaño de la pantalla');
